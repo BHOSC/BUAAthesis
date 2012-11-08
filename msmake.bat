5a52ae51588313fdaa5851bd996465102f5bf3d1
JosephPeng <mrpeng000@gmail.com>
2012-11-08 22:54:53 +0800

    change to utf-8 mode

--- msmake.bat
+++ msmake.bat
@@ -1,25 +1,24 @@
 ::======================================
-:: Filename:mamske.bat
-:: Author:  Joseph
-:: Created: 2012 - 07 - 12
-:: E-mail:  mrpeng000@gmail.com
+:: Filename: mamske.bat
+:: Author:   Joseph
+:: Created:  2012 - 07 - 12
+:: E-mail:   mrpeng000@gmail.com
 ::======================================
-::使用说明
+:: 使用说明
 ::
-::  1、生成文档
-::  bachelor|master [full]
-::    在当前目录下编译论文，将执行xelatex命令，若无相应的cls/bst文件，将直接报错
-::     - full 为首次编译或完全清空时的选项，将执行xelatex->bibtex->xelatex->xelatex命令
-::  2、清理文件
-::  clean [more]
-::    在当前目录下清理编译过程中产生的临时文件
-::     - more 将清除编译过程中产生的文件，包括pdf、aux、bbl等文件
+::   1、生成文档
+::   msmake [bachelor|master] [full]
+::     在当前目录下编译论文，将执行xelatex命令，若无相应的cls/bst文件，将直接报错
+::      - full 为首次编译或完全清空时的选项，将执行xelatex->bibtex->xelatex->xelatex命令
+::   2、清理文件
+::   msmake [clean] [more|empty]
+::     在当前目录下清理编译过程中产生的临时文件
+::     - more 将清除编译过程中产生的文件，包括*aux *.bbl 文件
+::     - empty 将清除编译过程中产生的文件，包括*aux *.bbl *.pdf 文件，不建议使用
 ::
 ::======================================
 
 @echo off
-::set mythesis=sample-bachelor
-::在此处更改需要编译的文件名
 :init
 if /i {%1}=={bachelor} goto thesis
 if /i {%1}=={master} goto thesis
@@ -29,7 +28,7 @@ if /i {%1}=={} goto help
 goto help
 
 ::======================================
-::编译我的论文
+:: 编译我的论文
 ::======================================
 :thesis
 echo 正在编译文件
@@ -37,6 +36,7 @@ if not exist buaathesis.cls goto clserr
 if not exist buaathesis.bst goto bsterr
 if /i {%1}=={bachelor} set mythesis=sample-bachelor
 if /i {%1}=={master} set mythesis=sample-master
+:: 如若主文件名更改，请将上面的"sample-bachelor"或"sample-master"更改。
 call xelatex %mythesis%
 if {%2}=={full} (goto full)
 if errorlevel 1 goto myerr1
@@ -54,7 +54,7 @@ call %mythesis%.pdf
 goto end
 
 ::======================================
-::清除文件以及清除更多文件
+:: 清除文件以及清除更多文件
 ::======================================
 :clean
 echo 删除编译临时文件
@@ -71,7 +71,7 @@ del /f /q /s *aux *.bbl *.pdf
 goto end
 
 ::======================================
-::帮助信息
+:: 帮助信息
 ::======================================
 :help
 echo            输入msmake+下面的命令，选择进入相应操作
@@ -83,11 +83,11 @@ echo	    help                  显示本帮助信息
 echo     bachelor/master full  为首次或运行"clean more"命令后使用
 echo	    clean more            将清除所有当前目录下的无关文件
 echo 注意：所需编译的文件名必须是sample-bachelor.tex或sample-master.tex
-::忍不住吐槽，为了显示对齐，居然排得这么乱！
+:: 忍不住吐槽，为了显示对齐，居然排得这么乱！
 goto end
 
 ::======================================
-::运行错误信息
+:: 运行错误信息
 ::======================================
 :myerr1
 echo 唉呀，生成论文失败了呢
@@ -103,6 +103,6 @@ echo 居然连bst的参考文献样式都木有！闹哪样！
 goto end
 
 ::======================================
-::结束符，无任何具体意义
+:: 结束符，无任何具体意义
 ::======================================
 :end
